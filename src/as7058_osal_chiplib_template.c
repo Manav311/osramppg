ea5d053d323d361ecd576f85aa7077253929bd6d
Manav Kashyap <108454508+Manav311@users.noreply.github.com>
2025-09-18 12:05:36 +0530

    INTERFACE NULL work

--- src/as7058_osal_chiplib_template.c
+++ src/as7058_osal_chiplib_template.c
@@ -45,16 +45,17 @@ struct device_config {
 
 /*! I2C address of the AS7058 */
 static const uint8_t g_i2c_address = 0x55;
-static const uint8_t l_i2c_address = 0x19;
-
-
 
 
 #define AS7058_NODE                   DT_ALIAS(as7058interrupt)
 
 #define LISDH12_NODE                   DT_ALIAS(lis2dh12interrupt)  
 
-struct gpio_dt_spec as7058_sensor_spec1 = GPIO_DT_SPEC_GET(AS7058_NODE, gpios); // Blue LED spec
+
+K_SEM_DEFINE(button_sem, 0, 1);
+
+static struct gpio_callback button_cb_data;
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET_OR(AS7058_NODE, gpios, {0}); // Button spec
 
 /*! Create internal instance of the device configuration */
 static struct device_config g_device_config;
@@ -71,6 +72,8 @@ static void interrupt_callback()
     err_code_t result;
     uint8_t pin_state = 1;
 
+    
+
     if (NULL != g_device_config.callback) {
         do {
             /* Calls the ChipLib callback function registered by as7058_osal_register_int_handler */
@@ -78,13 +81,18 @@ static void interrupt_callback()
 
             /* Read the pin state again because it could be high in meanwhile again */
             if (ERR_SUCCESS == result) {
-                 result = get_int_pin_state(&pin_state);
+                 result = gpio_pin_get_dt(&button);
             }
 
         } while ((ERR_SUCCESS == result) && pin_state);
     }
 }
 
+
+void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins) {
+
+	printk("Button pressed at %" PRIu32 "\n", k_cycle_get_32());
+}
 /******************************************************************************
  *                             GLOBAL FUNCTIONS                               *
  ******************************************************************************/
@@ -93,13 +101,40 @@ err_code_t as7058_osal_initialize(const char *p_interface_desc)
 {
     err_code_t result = ERR_SUCCESS;
 
+    
+
+    /* Shutdown OSAL interface in case there is one already opened */
+    if (g_device_config.init_done) {
+        as7058_osal_shutdown();
+    }
+    
 
      if (!device_is_ready(i2c_dev1)) {
 
-        as7058_osal_shutdown();
+         result = ERR_SYSTEM_CONFIG;
         /* handle error */
     }
-    g_device_config.init_done = true;
+  // Button initialization
+    if (!gpio_is_ready_dt(&button)) {
+        printk("Error: button device %s is not ready\n", button.port->name);
+        result = ERR_SYSTEM_CONFIG;
+    }
+
+    int ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+    if (ret < 0) {
+        printk("Error: failed to configure button pin\n");
+        result = ERR_SYSTEM_CONFIG;
+    }
+
+      gpio_init_callback(&button_cb_data, button_pressed, BIT(button.pin));
+    gpio_add_callback(button.port, &button_cb_data);
+
+   if (ERR_SUCCESS == result) {
+        g_device_config.init_done = TRUE;
+    } else {
+        as7058_osal_shutdown();
+    }
+
 
 
 
@@ -114,9 +149,7 @@ err_code_t as7058_osal_write_registers(uint8_t address,
         return ERR_PERMISSION;
     }
 
-    if (p_values == NULL) {
-        return ERR_POINTER;
-    }
+   M_CHECK_NULL_POINTER(p_values);
 
     /* Build [reg + data] buffer */
     uint8_t buf[number + 1];
@@ -124,6 +157,7 @@ err_code_t as7058_osal_write_registers(uint8_t address,
     memcpy(&buf[1], p_values, number);
 
     int ret = i2c_write(i2c_dev1, buf, number + 1, g_i2c_address);
+    
     if (ret < 0) {
         return ERR_DATA_TRANSFER;
     }
@@ -137,12 +171,9 @@ err_code_t as7058_osal_read_registers(uint8_t address, uint16_t number, uint8_t 
     if (false == g_device_config.init_done) {
         return ERR_PERMISSION;
     }
-    if (p_values == NULL) {
-        return ERR_POINTER;
-    }
-    if (!device_is_ready(i2c_dev1)) {
-        return ERR_DATA_TRANSFER;
-    }
+   M_CHECK_NULL_POINTER(p_values);
+
+
 
     /* Repeated-start: write 1 byte (reg addr), then read `number` bytes */
     int ret = i2c_write_read(i2c_dev1, g_i2c_address, &address, 1, p_values, number);
